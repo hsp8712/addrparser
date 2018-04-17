11787c3d4adddac62937760b7c94a09bba6a3235
hsp8712 <hsp8712@qq.com>
2018-04-17 14:41:38 +0800

    add china-region data; remove schema sql file
    Former-commit-id: ffacec259c3ac4f6215b499bb5360159cab62afa

--- src/main/resources/db/schema_mysql.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `region_data` (
-  `code` int(11) NOT NULL,
-  `parent_code` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `level` tinyint(4) NOT NULL,
-  `center` varchar(30) DEFAULT NULL,
-  `polyline` text NOT NULL,
-  PRIMARY KEY (`code`)
-);
